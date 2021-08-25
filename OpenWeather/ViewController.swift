78a5d43478176748ece13363b63c1fdda50ff399
gitnadia <nadia.g@inbox.lv>
2021-08-25 10:09:23 +0300

    Initial Commit

---
+++ OpenWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenWeather
+//
+//  Created by nadezda.gura on 25/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
